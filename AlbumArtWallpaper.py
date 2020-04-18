95aae7f2d9eb0ca4650c8fa7dd60a1b2860500cd
Will Raphaelson <wraphaelson@ILP01MBA005.internal.ngpvan.net>
2020-04-18 16:04:06 -0400

    taking out some prints

--- AlbumArtWallpaper.py
+++ AlbumArtWallpaper.py
@@ -30,14 +30,12 @@ def run_mac():
                 continue
             else:
                 old_filename = temp['item']['album']['images'][0]['url'].split("/")[-1]
-                print(old_filename)
                 old_absolute_filename = os.path.join(
                     os.path.abspath(os.getcwd()), old_filename)
                 os.system(f"rm {old_absolute_filename}")
                 link = user['item']['album']['images'][0]['url']
                 local_filename = wget.download(link)
                 full_local_filename = os.path.abspath(local_filename)
-                print(full_local_filename)
                 os.system(
                     f"osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"{full_local_filename}\"'")
                 time.sleep(5)
